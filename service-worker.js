279e6f10ecafcfcc3db0c8a8d4a2f5bef0a15004
Tim Franssen <tim.franssen@nedap.com>
2019-01-08 00:36:38 +0100

    Service worker fix..?

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,10 @@ self.addEventListener('install', function (event) {
   }));
 });
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
-    return response || fetch(event.request);
+  event.respondWith(caches.open('soundboard').then(function (cache) {
+    return cache.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    });
   }));
 });
 
